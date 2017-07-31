58c1a8193eb624dc9fda212284d66bea8c8987e7
jeevajeeva <vjeeva522@gmail.com>
2017-07-31 10:43:26 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,26 @@
+# mon1
+
+import java.io.*;
+import java.util.*;
+public class Swap
+{
+  public static void main(String args[])
+  {
+       Scanner sc=new Scanner(System.in);
+       String s=sc.nextLine();
+       int n=s.length();
+       //System.out.println(n);
+       if(n%2==0)
+       {
+         
+         System.out.println("Even String");
+         }
+         else
+         {
+            System.out.println("Odd String");
+           }
+
+
+    
+                    }
+}
